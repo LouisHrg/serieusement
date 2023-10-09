494f375648df4c7a03048a2b1a1d2fcf8749702a
Louis Harang <dev@narah.io>
2023-10-09 15:05:49 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -12,46 +12,56 @@
 	export let has2;
 	export let comment;
 	export let result;
-
-	function calculate() {
-		if(subject && action1 && action2 && action3 && action4 && search1 && search2 && has1 && has2 && comment){
-			result = `${subject} moi ? Sérieusement ^^ haha on me l avait pas sortie celle la depuis loooongtemps 🙂 demande a mes potes si je suis ${subject} tu vas voir les réponses que tu vas te prendre XD rien que la semaine passee j ai ${action1} dont chuuuuut ferme la ${subject} de merde car toi tu m as tout tout l air d un bon ${subject} de merde car souvent vous etes frustrer de ne pas ${action2} 🙂 ses agreable de ${action3} ou ${action4} hein ? tu peux pas répondre car tu ne sais pas ce que c ou alors tu le sais mais tu as du taper dans ta barre de recherche « ${search1} » ou « ${search2} » pour comprendre ce que c etait mdddrrr !! cest grave quoiquil en soit....pour revenir a moi, je pense que je suis le mec le moins ${subject} de ma bande de 11 meilleurs amis pas psk j ai eu le plus de ${has1} mais psk j ai eu ${has2} que mes amis 😀 ses pas moi qui le dit, ses eux qui commente sous mes photos insta « ${comment} notamment! »`;
-		}
-	}
 </script>
 <main>
-	<div class="container">
+	<div style="margin-left:5rem;margin-right:5rem; margin-top: 5rem;">
+		<div>
 		<h1>Sérieusement ^^</h1>
 		<h3>haha on me l avait pas sortie celle la depuis loooongtemps 🙂</h3>
+
+	</div>
 		<div class="row">
 			<div class="column">
-				<p>Qu'est ce que vous n'etes pas ? </p>
-				<input type="text" bind:value={subject} placeholder="Sujet"/>
+			<div class="row">
+				<div class="column">
+					<p>Qu'est ce que vous n'etes pas ? </p>
+					<input type="text" bind:value={subject} placeholder="Sujet"/>
+				</div>
+			</div>
+			<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous faites alors ? (que des verbes)</p>
+			<div class="row">
+				<input class="column" style="margin:10px" type="text" bind:value={action1} placeholder="Action 1"/>
+				<input class="column" style="margin:10px" type="text" bind:value={action2} placeholder="Action 2"/>
+				<input class="column" style="margin:10px" type="text" bind:value={action3} placeholder="Action 3"/>
+				<input class="column" style="margin:10px" type="text" bind:value={action4} placeholder="Action 4"/>
+			</div>
+			<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que les rageux recherchent ? </p>
+			<div class="row">
+				<input class="column" style="margin:10px" type="text" bind:value={search1} placeholder="Recherche 1"/>
+				<input class="column" style="margin:10px" type="text" bind:value={search2} placeholder="Recherche 2"/>
+			</div>
+			<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous n'avez pas plus que vos 11 meilleurs amis ? </p>
+			<input type="text" style="margin:10px" bind:value={has1}/>
+			<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous avez de plus que vos 11 meilleurs amis ? </p>
+			<input type="text" style="margin:10px" bind:value={has2}/>
+			<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vos 11 meilleurs amis commentent notamment ? </p>
+			<input type="text" style="margin:10px" bind:value={comment} placeholder="Commentaire instagram" />
+		</div>
+		<div class="column">
+			<div style="font-size:20px;margin:20px 50px;text-align: justify;">
+
+				<strong>{subject || '{sujet}'}</strong> moi ? Sérieusement ^^ haha on me l avait pas sortie celle la depuis loooongtemps 🙂 demande a mes potes si je suis <strong>{subject || '{sujet}'}</strong> tu vas voir les réponses que tu vas te prendre XD rien que la semaine passee j ai <strong>{action1 || '{action 1}'}</strong> dont chuuuuut ferme la <strong>{subject || '{sujet}'}</strong> de merde car toi tu m as tout tout l air d un bon <strong>{subject || '{sujet}'}</strong> de merde car souvent vous etes frustrer de ne pas <strong>{action2 || '{action 2}'}</strong> 🙂 ses agreable de <strong>{action3 || '{action 3}'}</strong> ou <strong>{action4 || '{action 4}'}</strong> hein ? tu peux pas répondre car tu ne sais pas ce que c ou alors tu le sais mais tu as du taper dans ta barre de recherche « <strong>{search1 || '{recherche 1}'}</strong> » ou « <strong>{search2|| '{recherche 2}'}</strong> » pour comprendre ce que c etait mdddrrr !! cest grave quoiquil en soit....pour revenir a moi, je pense que je suis le mec le moins <strong>{subject || '{sujet}'}</strong> de ma bande de 11 meilleurs amis pas psk j ai eu le plus de <strong>{has1 || '{truc que j\'ai eu 1}'}</strong> mais psk j ai eu <strong>{has2 || '{truc que j\'ai eu 2}'}</strong> que mes amis 😀 ses pas moi qui le dit, ses eux qui commente sous mes photos insta « <strong>{comment || '{commentaire}'}</strong> notamment! »
+			</div>
+				<div style="text-align: center;">
+					<img style="width:150px;border-radius: 100%; text-align: center;" src="/moi.png">
+				</div>
 			</div>
 		</div>
-		<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous faites alors ? (que des verbes)</p>
-		<div class="row">
-			<input class="column" style="margin:10px" type="text" bind:value={action1} placeholder="Action 1"/>
-			<input class="column" style="margin:10px" type="text" bind:value={action2} placeholder="Action 2"/>
-			<input class="column" style="margin:10px" type="text" bind:value={action3} placeholder="Action 3"/>
-			<input class="column" style="margin:10px" type="text" bind:value={action4} placeholder="Action 4"/>
-		</div>
-		<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que les rageux recherchent ? </p>
-		<div class="row">
-			<input class="column" style="margin:10px" type="text" bind:value={search1} placeholder="Recherche 1"/>
-			<input class="column" style="margin:10px" type="text" bind:value={search2} placeholder="Recherche 2"/>
-		</div>
-		<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous n'avez pas plus que vos 11 meilleurs amis ? </p>
-		<input type="text" style="margin:10px" bind:value={has1}/>
-		<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vous avez de plus que vos 11 meilleurs amis ? </p>
-		<input type="text" style="margin:10px" bind:value={has2}/>
-		<p style="margin-top: 20px; margin-bottom: 0">Qu'est ce que vos 11 meilleurs amis commentent notamment ? </p>
-		<input type="text" style="margin:10px" bind:value={comment} placeholder="Commentaire instagram" />
-		<br>
-		<button class="btn" style="width:100%; margin-top:20px; margin-bottom: 10px" on:click={calculate}> Générer </button>
-		<p style="text-align: center;">Résultat : </p>
-		{#if result}
-		<p> { result } </p>
-		{/if}
 	</div>
 </main>
+
+<style scoped>
+	strong {
+		color: #cf2f74;
+	}
+</style>
